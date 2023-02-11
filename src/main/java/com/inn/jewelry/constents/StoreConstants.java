b2c7c35f054661e06fb6d3646e9a4789e838f41b
krupk <MaxKR1994>
2023-02-12 00:21:41 +0200

    added exception messages

--- src/main/java/com/inn/jewelry/constents/StoreConstants.java
+++ src/main/java/com/inn/jewelry/constents/StoreConstants.java
@@ -2,7 +2,9 @@ package com.inn.jewelry.constents;
 
 public class StoreConstants {
 
-    public static final String SOMETHING_WENT_WRONG="Something Went Wrong";
-    public static final String INVALID_DATA="Invalid Data";
+    public static final String SOMETHING_WENT_WRONG="Something Went Wrong (";
+    public static final String INVALID_DATA="Invalid Data (";
+    public static final String EMAIL_ALREADY_EXIST="Email Already Exist. Try Different Email.";
+    public static final String SUCCESSFULLY_REGISTERED="Successfully registered !";
 
 }
