634af5b0eb6b9afab785cf133e8cadde9b2aa885
krupk <MaxKR1994>
2023-02-11 15:29:54 +0200

    created class with exceptions messages

---
+++ src/main/java/com/inn/jewelry/constents/StoreConstants.java
@@ -0,0 +1,8 @@
+package com.inn.jewelry.constents;
+
+public class StoreConstants {
+
+    public static final String SOMETHING_WENT_WRONG="Something Went Wrong";
+    public static final String INVALID_DATA="Invalid Data";
+
+}
