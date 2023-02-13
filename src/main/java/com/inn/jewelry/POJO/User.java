c0c0525b62da4a5f28c73ee22ac49674e9f7b7d7
krupk <MaxKR1994>
2023-02-13 12:30:26 +0200

    added new query

--- src/main/java/com/inn/jewelry/POJO/User.java
+++ src/main/java/com/inn/jewelry/POJO/User.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @NamedQuery(name = "User.findByEmailId", query = "select u from User u where u.email =: email")
+@NamedQuery(name = "User.getAllUser", query = "select new com.inn.jewelry.wrapper.UserWrapper(u.id, u.name, u.email, u.contactnumber, u.status) from User u where u.role='user'")
 @Data
 @Entity
 @DynamicUpdate
