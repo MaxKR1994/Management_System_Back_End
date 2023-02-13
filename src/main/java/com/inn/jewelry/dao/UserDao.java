65a1a83d7aa5da4ef870b5645ec07bbbbf23fd88
krupk <MaxKR1994>
2023-02-13 16:09:56 +0200

    added new method

--- src/main/java/com/inn/jewelry/dao/UserDao.java
+++ src/main/java/com/inn/jewelry/dao/UserDao.java
@@ -13,9 +13,9 @@ public interface UserDao extends JpaRepository<User,Integer> {
 
     User findByEmailId(@Param("email") String email);
     List<UserWrapper> getAllUser();
-
     @Transactional
     @Modifying
     Integer updateStatus(@Param("status")String status,@Param("id") Integer id);
+    List<String> getAllAdmin();
 
 }
