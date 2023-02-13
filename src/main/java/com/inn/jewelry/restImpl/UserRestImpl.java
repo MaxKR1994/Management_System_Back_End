5c8ccc4f183f3ac40fbbb8d5f5bdd332f8623f60
krupk <MaxKR1994>
2023-02-13 03:15:14 +0200

    added postMapping for login

--- src/main/java/com/inn/jewelry/restImpl/UserRestImpl.java
+++ src/main/java/com/inn/jewelry/restImpl/UserRestImpl.java
@@ -30,4 +30,14 @@ public class UserRestImpl implements UserRest {
             return StoreUtils.getResponseEntity(StoreConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @Override
+    public ResponseEntity<String> login(Map<String, String> requestMap) {
+        try{
+            return userService.login(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return StoreUtils.getResponseEntity(StoreConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
