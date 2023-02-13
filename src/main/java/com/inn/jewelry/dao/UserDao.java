86a5572ee7aab9138c838d52c78bd18bf5747cad
krupk <MaxKR1994>
2023-02-13 12:11:14 +0200

    added getAllUser()

--- src/main/java/com/inn/jewelry/dao/UserDao.java
+++ src/main/java/com/inn/jewelry/dao/UserDao.java
@@ -1,11 +1,16 @@
 package com.inn.jewelry.dao;
 
 import com.inn.jewelry.POJO.User;
+import com.inn.jewelry.wrapper.UserWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserDao extends JpaRepository<User,Integer> {
 
     User findByEmailId(@Param("email") String email);
+
+    List<UserWrapper> getAllUser();
 
 }
