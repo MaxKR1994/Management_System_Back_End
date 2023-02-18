82a6c1fd8f34f191ea6f50372cff691c05b9fe8b
krupk <MaxKR1994>
2023-02-19 01:36:32 +0200

    added getAllProduct method to repository

--- src/main/java/com/inn/jewelry/dao/ProductDao.java
+++ src/main/java/com/inn/jewelry/dao/ProductDao.java
@@ -1,8 +1,12 @@
 package com.inn.jewelry.dao;
 
 import com.inn.jewelry.POJO.Product;
+import com.inn.jewelry.wrapper.ProductWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product,Integer> {
 
+    List<ProductWrapper> getAllProduct();
 }
