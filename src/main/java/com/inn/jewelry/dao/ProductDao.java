b482723615dd77a68f02ab0768565697f1cd92af
krupk <MaxKR1994>
2023-02-19 14:39:01 +0200

    created updateStatus() query

--- src/main/java/com/inn/jewelry/dao/ProductDao.java
+++ src/main/java/com/inn/jewelry/dao/ProductDao.java
@@ -3,10 +3,17 @@ package com.inn.jewelry.dao;
 import com.inn.jewelry.POJO.Product;
 import com.inn.jewelry.wrapper.ProductWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product,Integer> {
 
     List<ProductWrapper> getAllProduct();
+
+    @Modifying
+    @Transactional
+    Integer updateProductStatus(@Param("status") String status,@Param("id") Integer id);
 }
