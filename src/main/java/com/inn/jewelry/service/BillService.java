bdfa592f9a1483d5ee7d57450ad6713903a3b8dc
krupk <MaxKR1994>
2023-02-22 01:15:38 +0200

    created two methods getBills() and getPdf()

--- src/main/java/com/inn/jewelry/service/BillService.java
+++ src/main/java/com/inn/jewelry/service/BillService.java
@@ -1,9 +1,15 @@
 package com.inn.jewelry.service;
 
+import com.inn.jewelry.POJO.Bill;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
 import java.util.Map;
 
 public interface BillService {
     ResponseEntity<String> generateReport(Map<String, Object> requestMap);
+
+    ResponseEntity<List<Bill>> getBills();
+
+    ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap);
 }
