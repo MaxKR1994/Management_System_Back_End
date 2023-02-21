a1f3a7c8505679fb2dd33a3246c7d65e1605419a
krupk <MaxKR1994>
2023-02-22 01:18:54 +0200

    implemented getBills() and getPdf()

--- src/main/java/com/inn/jewelry/restImpl/BillRestImpl.java
+++ src/main/java/com/inn/jewelry/restImpl/BillRestImpl.java
@@ -1,5 +1,6 @@
 package com.inn.jewelry.restImpl;
 
+import com.inn.jewelry.POJO.Bill;
 import com.inn.jewelry.constents.StoreConstants;
 import com.inn.jewelry.rest.BillRest;
 import com.inn.jewelry.service.BillService;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,5 +28,25 @@ public class BillRestImpl implements BillRest {
             ex.printStackTrace();
         }
         return StoreUtils.getResponseEntity(StoreConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<Bill>> getBills() {
+        try{
+            return billService.getBills();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap) {
+        try{
+            return billService.getPdf(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
